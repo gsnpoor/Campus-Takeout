c73bde7d2c0b7e95cb35b30283589470de3e1f59
gsnpoor <2366106257@qq.com>
2023-04-23 22:16:35 +0800

    2023.4.23

---
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,68 @@
+package com.itheima.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //1、获取本次请求的URI
+        String requestURI = request.getRequestURI();
+        log.info("requestURI: {}",requestURI);
+
+        //定义不需要处理的请求路径
+        String[] urls = new String[] {
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        //2、判断本次请求是否需要处理
+        boolean check = check(urls, requestURI);
+
+        //3、如果不需要处理，则直接放行
+        if (check) {
+            log.info("本次请求{}不需要处理",requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //4、判断登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("employee") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        log.info("用户未登录");
+        //5、如果未登录则返回未登灵结果
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+        return;
+    }
+
+    //检查本次请求是否放行
+    public boolean check(String[] urls, String requestURI){
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
